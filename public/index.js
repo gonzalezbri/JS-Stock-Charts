7cb7cd7455174975d2af41b6bf14e543b707688e
gonzalezbri <4717bg@gmail.com>
2023-03-22 20:03:31 -0700

    added highest price chart and function

--- public/index.js
+++ public/index.js
@@ -1,9 +1,67 @@
-async function main() {
-
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
 }
 
-main()
+async function main() {
+const timeChartCanvas = document.querySelector('#time-chart');
+const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
+const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=3530d8641a214c459d7a41948be72ad8`)
+
+const result = await response.json()
+
+const { GME, MSFT, DIS, BNTX } = result;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+stocks.forEach( stock => stock.values.reverse())
+
+
+new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    })    
+    ;
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label:stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    })
+};
+function findHighest(values) {
+    let highest = 0;
+    values.forEach(value => {
+        if (parseFloat(value.high) > highest) {
+            highest = value.high
+        }
+    })
+    return highest
+}
+main();
